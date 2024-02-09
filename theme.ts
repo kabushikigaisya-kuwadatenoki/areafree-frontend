286766a0a3c17399d3b07f4853d3b8c30e91db0f
ひらぬま <shuuuya0616@gmail.com>
2024-02-09 12:43:52 +0900

    mantineに独自themeを実装

--- theme.ts
+++ theme.ts
@@ -1,7 +1,25 @@
-"use client";
+'use client'
 
-import { createTheme } from "@mantine/core";
+import { createTheme } from '@mantine/core'
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
-});
+  primaryColor: 'primary',
+  colors: {
+    primary: [
+      '#f7f5f2',
+      '#eae9e7',
+      '#d4cfca',
+      '#beb4a9',
+      '#ac9d8d',
+      '#a28f7a',
+      '#9d8870',
+      '#88755e',
+      '#7a6852',
+      '#6b5842',
+    ],
+  },
+  headings: {
+    fontWeight: '400',
+  },
+})
