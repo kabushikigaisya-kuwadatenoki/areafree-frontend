2d4c7b02f66ab7eb346f3169da177b5c80e63dd4
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-14 11:52:25 +0900

    広告、フッターを作成

---
+++ app/_components/ui/common/footer.tsx
@@ -0,0 +1,34 @@
+import { Flex, SimpleGrid, Text } from '@mantine/core'
+import Link from 'next/link'
+
+export function Footer() {
+  return (
+    <>
+      <Flex h="95px" align="center" justify="center" bg="url(/advertise-image.png)" bgsz="contain">
+        <Text c="white" size="40px" fw={700}>
+          広告
+        </Text>
+      </Flex>
+      <SimpleGrid cols={{ base: 2 }} spacing={{ base: 6 }} p="md">
+        <Text size="xs" fw={700} component={Link} href="/hoge">
+          当システムの使用方法
+        </Text>
+        <Text size="xs" fw={700} component={Link} href="/hoge">
+          利用規約
+        </Text>
+        <Text size="xs" fw={700} component={Link} href="/hoge">
+          お問合せはこちら
+        </Text>
+        <Text size="xs" fw={700} component={Link} href="/hoge">
+          個人情報の取り扱いについて
+        </Text>
+        <Text size="xs" fw={700} component={Link} href="/hoge">
+          FAQ
+        </Text>
+        <Text size="xs" fw={700} component={Link} href="/hoge">
+          運営会社
+        </Text>
+      </SimpleGrid>
+    </>
+  )
+}
