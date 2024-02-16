85677b26f134b32d0c1df92c303e6206035d5dda
Ryo-1157 <hit.zjackpot.777@gmail.com>
2024-02-16 17:03:26 +0900

    トップページへのリンク作成

--- app/_components/ui/common/authed-header.tsx
+++ app/_components/ui/common/authed-header.tsx
@@ -1,6 +1,7 @@
 import { Flex, Paper } from '@mantine/core'
 import { IconBell, IconLogout, IconUserCircle } from '@tabler/icons-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const logo = '/logo.png'
 
@@ -8,7 +9,9 @@ export function AuthedHeader() {
   return (
     <Paper shadow="lg" px="md">
       <Flex justify="space-between" align="center" h="52px">
+        <Link href="/">
         <Image src={logo} width={85} height={37} alt="logo" />
+        </Link>
         <Flex gap="sm" align="center">
           <IconBell size={24} strokeWidth={2} color={'#F59E0B'} />
           <IconUserCircle size={24} strokeWidth={2} color={'#555555'} />
--- app/_components/ui/common/unauth-header.tsx
+++ app/_components/ui/common/unauth-header.tsx
@@ -1,11 +1,15 @@
 import { Flex, Paper } from '@mantine/core'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const logo = '/logo.png'
 
 export function UnAuthHeader() {
   return (
     <Paper shadow="lg" px="md">
+              <Link href="/">
+        <Image src={logo} width={85} height={37} alt="logo" />
+        </Link>
       <Flex justify="space-between" align="center" h="52px">
         <Image src={logo} width={85} height={37} alt="logo" />
       </Flex>
