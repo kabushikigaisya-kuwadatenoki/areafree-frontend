4a526673185abfff6c669601baa8f924cf33da1f
Ryo-1157 <hit.zjackpot.777@gmail.com>
2024-02-16 17:02:55 +0900

    メタタグ変更

--- app/(unauthenticated)/layout.tsx
+++ app/(unauthenticated)/layout.tsx
@@ -3,8 +3,8 @@ import { UnAuthHeader } from '@/app/_components/ui/common/unauth-header'
 import React from 'react'
 
 export const metadata = {
-  title: 'Exampleページ',
-  description: 'Exampleページだよん',
+  title: 'test',
+  description: 'testpage',
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
