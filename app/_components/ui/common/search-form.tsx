626431c8bd15b3a8b417425da224e1ba6f96118b
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-19 18:00:03 +0900

    検索欄にお気に入りフィルターを設定

--- app/_components/ui/common/search-form.tsx
+++ app/_components/ui/common/search-form.tsx
@@ -1,32 +1,64 @@
 'use client'
-import { Group, TextInput } from '@mantine/core'
+import { Box, Checkbox, Group, TextInput } from '@mantine/core'
 import { IconSearch } from '@tabler/icons-react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
+import React from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 
-export function SearchBar() {
+export function SearchForm() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
+  const [checked, setChecked] = useState(false)
 
-  const handleSearch = useDebouncedCallback((term: string) => {
+  const updateQueryParams = useDebouncedCallback((term: string, favorites: boolean) => {
     const params = new URLSearchParams(searchParams)
+
     if (term) {
       params.set('query', term)
     } else {
       params.delete('query')
     }
+
+    if (favorites) {
+      params.set('favorites', 'true')
+    } else {
+      params.delete('favorites')
+    }
+
     replace(`${pathname}?${params.toString()}`)
   }, 300)
 
+  const handleSearchChange = (term: string) => {
+    updateQueryParams(term, checked)
+  }
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.currentTarget.checked)
+    updateQueryParams(searchParams.get('query') || '', event.currentTarget.checked)
+  }
+
   return (
-    <Group justify="center">
-      <IconSearch size={24} />
-      <TextInput
-        placeholder="フリーワード"
-        onChange={(event) => handleSearch(event.currentTarget.value)}
-        defaultValue={searchParams.get('query') || ''}
-      />
-    </Group>
+    <>
+      <Box maw={290} mx="auto">
+        <Group justify="space-between">
+          <IconSearch size={24} />
+          <TextInput
+            w="85%"
+            placeholder="フリーワード"
+            onChange={(event) => handleSearchChange(event.currentTarget.value)}
+            defaultValue={searchParams.get('query') || ''}
+            mb="sm"
+          />
+        </Group>
+        <Checkbox
+          label="お気に入り登録済みガイドのみを表示"
+          checked={checked}
+          onChange={handleCheckboxChange}
+          size="xs"
+        />
+      </Box>
+    </>
   )
 }
