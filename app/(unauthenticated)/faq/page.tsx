2ba965520f720b4c16d2968e34d7e2d37b05bc3e
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-19 14:33:39 +0900

    FAQページ作成

---
+++ app/(unauthenticated)/faq/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { Accordion, Group, Paper, Text, Title } from '@mantine/core'
+import { IconChevronDown } from '@tabler/icons-react'
+
+export default function Page() {
+  const faq = [
+    {
+      question: 'ふぁふぁふぁふぁっふぁふぁふ',
+      answer:
+        'ふぁふぁふぁふぁふぁふぁっふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁっふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁっふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁっふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁっふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁふぁっふぁふぁふぁふぁふぁふぁふぁ',
+    },
+    {
+      question: '質問2',
+      answer: '回答2',
+    },
+  ]
+  const items = faq.map((item) => (
+    <Paper p="md" shadow="md" maw={290} mx="auto" my={10}>
+      <Accordion.Item key={item.question} value={item.question}>
+        <Accordion.Control chevron={<IconChevronDown color="#33947D" />}>
+          <Group justify="flex-start" gap={10} align="center">
+            <Text size="xs">Q.</Text>
+            <Text size="10px" maw={170}>
+              {item.question}
+            </Text>
+          </Group>
+        </Accordion.Control>
+        <Accordion.Panel>
+          <Group justify="flex-start" gap={10}>
+            <Text size="xs">A.</Text>
+            <Text size="10px">{item.answer}</Text>
+          </Group>
+        </Accordion.Panel>
+      </Accordion.Item>
+    </Paper>
+  ))
+  return (
+    <>
+      <Title size="sm" fw={700} ta="center" my="md">
+        FQA
+      </Title>
+      <Accordion>{items}</Accordion>
+    </>
+  )
+}
