c434bee02f1b8469cab76722c4e96d8ac5f80741
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-19 17:59:51 +0900

    レイアウト更新

--- app/(authenticated)/guide/page.tsx
+++ app/(authenticated)/guide/page.tsx
@@ -1,5 +1,13 @@
 import { GuideTabs } from '@/app/(authenticated)/_components/guide/tabs'
+import { SearchForm } from '@/app/_components/ui/common/search-form'
+import { Checkbox, Rating, Select, Text } from '@mantine/core'
+import Image from 'next/image'
 
 export default function Page() {
-  return <GuideTabs />
+  return (
+    <>
+      <GuideTabs />
+      <SearchForm />
+    </>
+  )
 }
