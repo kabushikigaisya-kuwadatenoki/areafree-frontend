8ee91a4b090149ac08bb6deb4486c837a646e94e
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-19 13:29:56 +0900

    お問い合わせページ修正

---
+++ app/(authenticated)/inquiry/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { ComponentWrapper } from '@/app/_components/ui/common/component-wrapper'
+import { Button, Group, Textarea, Title } from '@mantine/core'
+import { useForm } from '@mantine/form'
+
+export default function Page() {
+  const form = useForm({
+    initialValues: {
+      inquiry: '',
+    },
+  })
+
+  return (
+    <ComponentWrapper>
+      <Title size="md" fw={700} ta="center" mb="md">
+        お問合わせフォーム
+      </Title>
+      <form action="">
+        <Textarea
+          label="お問合せ内容"
+          required
+          autosize
+          minRows={10}
+          {...form.getInputProps('inquiry')}
+        />
+        <Group justify="center" mt="md">
+          <Button type="submit">送信</Button>
+        </Group>
+      </form>
+    </ComponentWrapper>
+  )
+}
