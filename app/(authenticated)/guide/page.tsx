3d763c049440b36bc751221d6bd49484c5a3b86b
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-22 14:54:40 +0900

    情報の配信

--- app/(authenticated)/guide/page.tsx
+++ app/(authenticated)/guide/page.tsx
@@ -1,15 +1,41 @@
 import { DetailFilter } from '@/app/(authenticated)/_components/guide/detail-filter'
-import { GuideTabs } from '@/app/(authenticated)/_components/guide/tabs'
+import { GuideCard } from '@/app/(authenticated)/_components/guide/guide-card'
+import { UserTabs } from '@/app/(authenticated)/_components/guide/user-tabs'
 import { SearchForm } from '@/app/_components/ui/common/search-form'
-import { Checkbox, Rating, Select, Text } from '@mantine/core'
-import Image from 'next/image'
+import { LoadingOverlay } from '@mantine/core'
+import { Paper } from '@mantine/core'
+import { Suspense } from 'react'
+
+const guide_dummy = {
+  id: '1',
+  nickname: 'TaroTokyo',
+  evaluation: 3.7,
+  created_at: '2024-02-22T15:00:00Z',
+  available_languages: ['English', 'Japanese'],
+  comment: 'Very knowledgeable and friendly guide!',
+  favorite: true,
+  profile_image: '/prof-dummy.png',
+  address: '東京都新宿区',
+}
+
+const GuideIndexItems = () => {
+  return (
+    <>
+      <Paper bg="#CDE8E2" p="xs">
+        <SearchForm />
+        <DetailFilter />
+      </Paper>
+      <Suspense key={guide_dummy.id} fallback={<LoadingOverlay />}>
+        <GuideCard guide={guide_dummy} />
+      </Suspense>
+    </>
+  )
+}
 
 export default function Page() {
   return (
     <>
-      <GuideTabs />
-      <SearchForm />
-      <DetailFilter />
+      <UserTabs indexGuideComponents={<GuideIndexItems />} />
     </>
   )
 }
