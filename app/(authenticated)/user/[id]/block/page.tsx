339bfa90f1da2f888cddbaf283e81751b64d1db7
Ryo-1157 <hit.zjackpot.777@gmail.com>
2024-02-26 17:46:46 +0900

    ブロック一覧ページ作成

--- app/(authenticated)/user/[id]/block/page.tsx
+++ app/(authenticated)/user/[id]/block/page.tsx
@@ -1,3 +1,41 @@
+import { BlockedGuideCard } from './_components/blocked-guide-card'
+
 export default function Page({ params }: { params: { id: number } }) {
-  return <p>this is Block page, UserId: {params.id}</p>
+  const guide_dummy = [
+    {
+      id: 1,
+      nickname: 'TaroTokyo',
+      evaluation: 3.7,
+      created_at: '2024-02-22',
+      available_languages: ['English', 'Japanese'],
+      comment: 'Very knowledgeable and friendly guide!',
+      is_favorite: true,
+      profile_image: '/prof-dummy.png',
+      address: '東京都新宿区',
+    },
+    {
+      id: 2,
+      nickname: 'TaroTokyo',
+      evaluation: 3.7,
+      created_at: '2024-02-22',
+      available_languages: ['English', 'Japanese'],
+      comment: 'Very knowledgeable and friendly guide!',
+      is_favorite: true,
+      profile_image: '/prof-dummy.png',
+      address: '東京都新宿区',
+    },
+    {
+      id: 3,
+      nickname: 'TaroTokyo',
+      evaluation: 3.7,
+      created_at: '2024-02-22',
+      available_languages: ['English', 'Japanese'],
+      comment: 'Very knowledgeable and friendly guide!',
+      is_favorite: true,
+      profile_image: '/prof-dummy.png',
+      address: '東京都新宿区',
+    },
+  ]
+
+  return <BlockedGuideCard guides={guide_dummy} />
 }
