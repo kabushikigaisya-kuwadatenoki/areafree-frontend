76d4aefcfc7225800e6cf2d638f4e62e5d21a5c9
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-03-01 08:39:06 +0900

    Link先をparamsで設定

--- app/_components/ui/common/authed-header.tsx
+++ app/_components/ui/common/authed-header.tsx
@@ -5,15 +5,17 @@ import Link from 'next/link'
 
 const logo = '/logo.png'
 
-export function AuthedHeader() {
+export function AuthedHeader(params: { userId: string }) {
   return (
     <Paper shadow="lg" px="md">
       <Flex justify="space-between" align="center" h="52px">
         <Link href="/">
-        <Image src={logo} width={85} height={37} alt="logo" />
+          <Image src={logo} width={85} height={37} alt="logo" />
         </Link>
         <Flex gap="sm" align="center">
-          <IconBell size={24} strokeWidth={2} color={'#F59E0B'} />
+          <Link href={`/user/${params.userId}`}>
+            <IconBell size={24} strokeWidth={2} color={'#F59E0B'} />
+          </Link>
           <IconUserCircle size={24} strokeWidth={2} color={'#555555'} />
           <IconLogout size={24} strokeWidth={2} color="#A0AEC0" />
         </Flex>
