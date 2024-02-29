a6a14a1871b26e27c56a4b01eab7f6f3b7e9e545
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-03-01 08:38:07 +0900

    Dynamic Routingを修正。URLを修正

--- app/(authenticated)/user/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { UserProfileForm } from '@/app/_components/ui/forms/user-profile-form'
-
-export default function Page({ params }: { params: { id: number } }) {
-  return <UserProfileForm />
-}
