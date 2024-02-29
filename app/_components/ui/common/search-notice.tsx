005d8b2243f76bd3dde133da375aa386be172a2b
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-02-29 18:08:54 +0900

    検索コンポーネント作成

--- app/_components/ui/common/search-notice.tsx
+++ app/_components/ui/common/search-notice.tsx
@@ -1,13 +1,12 @@
 'use client'
-import { Box, Button, Group, TextInput } from '@mantine/core'
+import { Box, Group, TextInput } from '@mantine/core'
 import { IconSearch } from '@tabler/icons-react'
-import Link from 'next/link'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useState } from 'react'
 import React from 'react'
 import { useDebouncedCallback } from 'use-debounce'
 
-export function SearchReport() {
+export function SearchNotice() {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
@@ -38,19 +37,14 @@ export function SearchReport() {
   return (
     <>
       <Box mx="auto" bg="#CDE8E2" py={5} px={10}>
-        <Group gap={0} justify="space-between" align="center">
-          <Group align="center" gap={5}>
-            <IconSearch size={24} />
-            <TextInput
-              w="80%"
-              placeholder="フリーワード"
-              onChange={(event) => handleSearchChange(event.currentTarget.value)}
-              defaultValue={searchParams.get('query')?.toString() || ''}
-            />
-          </Group>
-          <Button mb={0} size="sm" component={Link} href="/report">
-            凍結一覧
-          </Button>
+        <Group w="100%" gap={5} align="center">
+          <IconSearch size={24} />
+          <TextInput
+            w="80%"
+            placeholder="フリーワード"
+            onChange={(event) => handleSearchChange(event.currentTarget.value)}
+            defaultValue={searchParams.get('query')?.toString() || ''}
+          />
         </Group>
       </Box>
     </>
