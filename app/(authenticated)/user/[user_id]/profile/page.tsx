12762d513247b63d4cd073dd2adcfb54417d8d70
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-03-01 11:55:50 +0900

    プロフィール情報入力フォーム作成

--- app/(authenticated)/user/[user_id]/profile/page.tsx
+++ app/(authenticated)/user/[user_id]/profile/page.tsx
@@ -1,5 +1,5 @@
 import { UserProfileForm } from '@/app/_components/ui/forms/user-profile-form'
 
-export default function Page({ params }: { params: { id: number } }) {
+export default function Page({ params }: { params: { user_id: number } }) {
   return <UserProfileForm />
 }
