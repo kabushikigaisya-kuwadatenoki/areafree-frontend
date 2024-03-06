4e91edd27ff9aa8606c18b870efcb8e47e05eb6d
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-03-06 18:47:45 +0900

    リダイレクトの処理をいったん削除

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,29 +1,16 @@
-"use client";
-
 import { AuthedHeader } from '@/app/_components/ui/common/authed-header';
 import { Footer } from '@/app/_components/ui/common/footer';
 import Cookies from 'js-cookie';
-import { usePathname, useRouter } from 'next/navigation';
-import React, { useEffect } from 'react';
-
+import { redirect } from "next/navigation"
+import React from "react"
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: Props) {
-  const router = useRouter()
-  const pathname = usePathname()
 
-  useEffect(() => {
-    // アクセストークンの存在を確認
-    const accessToken = Cookies.get('accessToken');
 
-    // アクセストークンがなければログインページにリダイレクト
-    if (!accessToken && pathname !== '/login') {
-      router.push('/login');
-    }
-  }, [router, pathname]);
 
   return (
     <>
