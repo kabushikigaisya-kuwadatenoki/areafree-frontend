8fbd4f333d0491bc84fa897a217f821439a3ad3d
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-03-06 18:49:37 +0900

    Propsの変更にともなう値変更

--- app/(authenticated)/_components/user/guide-card-detail.tsx
+++ app/(authenticated)/_components/user/guide-card-detail.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { formatDate } from '@/app/_functions/format-date'
 import { Box, Card, Group, Rating, Text } from '@mantine/core'
 import { IconStar } from '@tabler/icons-react'
 import Image from 'next/image'
@@ -6,7 +7,7 @@ import Link from 'next/link'
 
 type Guide = {
   id: string
-  nickname: string
+  guide_nickname: string
   evaluation: number
   created_at: string
   available_languages: string[]
@@ -25,17 +26,17 @@ export function GuideCardDetail({ guides }: Props) {
   return (
     <>
       {guidesArray.map((item) => (
-        <Card withBorder maw={352} mx="auto" shadow="xs" p="md" mt="xs" radius="md" key={item.id}>
+        <Card withBorder maw={375} mx="auto" shadow="xs" p="md" mt="xs" radius="md" key={item.id}>
           <Group>
-            <Image src={item.profile_image} alt={item.nickname} width={87} height={76} />
-            <Box w="60%">
+            <Image src={item.profile_image} alt={item.guide_nickname} width={87} height={76} />
+            <Box w="50%">
               <Text size="10px" mb="xs">
                 ガイド評価
               </Text>
               <Rating size="xs" value={item.evaluation} fractions={4} />
               <Group>
                 <Text size="md" fw={700} mb="sm">
-                  {item.nickname}
+                  {item.guide_nickname}
                 </Text>
                 <Link href={`${item.id}/evaluation`}>
                   <IconStar fontWeight={100} width={16} height={16} />
@@ -45,15 +46,15 @@ export function GuideCardDetail({ guides }: Props) {
                 <Box>
                   <Text size="10px">ガイド歴</Text>
                   <Text size="10px" mt="5px">
-                    {item.created_at}
+                    {formatDate(item.created_at)}
                   </Text>
                 </Box>
                 <Box>
                   <Text size="10px">対応言語</Text>
                   <Group>
-                    {item.available_languages.map((item) => (
-                      <Text size="10px" mt="5px" key={item}>
-                        {item}
+                    {item.available_languages.map((language) => (
+                      <Text size="10px" mt="5px" key={language}>
+                        {language}
                       </Text>
                     ))}
                   </Group>
