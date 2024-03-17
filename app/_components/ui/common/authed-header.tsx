53009a5789b5e80547a989426f74b2b4677c4a71
R.Yamaga <hit.zjackpot.777@gmail.com>
2024-03-17 21:16:20 +0900

    admin時遷移先を変更

--- app/_components/ui/common/authed-header.tsx
+++ app/_components/ui/common/authed-header.tsx
@@ -16,6 +16,7 @@ export function AuthedHeader() {
   const router = useRouter();
   const [userId, setUserId] = useState<string | undefined>();
   const [guideId, setGuideId] = useState<string | undefined>();
+  const [admin, setAdmin] = useState<boolean | undefined>();
 
   const logout = () => {
     Cookies.remove('accessToken')
@@ -27,17 +28,25 @@ export function AuthedHeader() {
   useEffect(() => {
     const accessToken = Cookies.get("accessToken");
     if (accessToken) {
-      const decodedToken = jwt.decode(accessToken) as { user_id: string, guide_id: string };
+      const decodedToken = jwt.decode(accessToken) as { user_id: string, guide_id: string, is_admin: boolean };
       setUserId(decodedToken.user_id);
       setGuideId(decodedToken.guide_id);
+      setAdmin(decodedToken.is_admin)
     }
   }, []);
   return (
     <Paper shadow="lg" px="md">
       <Flex justify="space-between" align="center" h="52px">
-        <Link href={`/user/${userId}`}>
-          <Image src={logo} width={85} height={37} alt="logo" />
-        </Link>
+        {
+          admin ? (
+            <Link href={"/admin"}>
+              <Image src={logo} width={85} height={37} alt="logo" />
+            </Link>
+          ) : (
+            <Link href={`/user/${userId}`}>
+              <Image src={logo} width={85} height={37} alt="logo" />
+            </Link>
+          )}
         <Flex gap="sm" align="center">
           {guideId ? (
             <Link href={`/guide/${guideId}/`}>
